dbb1d1441e235b6deddce43c4d58cb3c5d5715df
Martijn Arts <martijn@martijnarts.com>
2017-11-22 13:44:40 +0100

    Send events one call after renaming file

--- send.php
+++ send.php
@@ -29,64 +29,73 @@ if ($file[0] != '/') $file = __DIR__ . "/" . $file;
 
 $dir = dirname($file);
 $old = $file;
-$file = $dir . '/analytics-' . rand() . '.log';
 
-if(!file_exists($old)) {
-  print("file: $old does not exist");
-  exit(0);
+$newFileName = 'analytics-' . rand() . '.log';
+$newFile = $dir . '/' . $newFileName;
+
+if(file_exists($old)) {
+    if (!rename($old, $newFile)) {
+        print("error renaming from $old to $newFile\n");
+        exit(1);
+    }
 }
 
-if (!rename($old, $file)) {
-  print("error renaming from $old to $file\n");
-  exit(1);
+foreach(scandir($dir) as $file) {
+    if (substr($file, 0, strlen('analytics-')) !== 'analytics-') {
+        continue;
+    }
+    if ($file === $newFileName) {
+        continue;
+    }
+
+    /**
+    * File contents.
+    */
+
+    $contents = file_get_contents($file);
+    $lines = explode("\n", $contents);
+
+    /**
+    * Initialize the client.
+    */
+
+    Segment::init($args["secret"], array(
+        "debug" => true,
+        "error_handler" => function($code, $msg){
+            print("$code: $msg\n");
+            exit(1);
+        }
+    ));
+
+    /**
+    * Payloads
+    */
+
+    $total = 0;
+    $successful = 0;
+    foreach ($lines as $line) {
+        if (!trim($line)) continue;
+        $payload = json_decode($line, true);
+        $dt = new DateTime($payload["timestamp"]);
+        $ts = floatval($dt->getTimestamp() . "." . $dt->format("u"));
+        $payload["timestamp"] = $ts;
+        $type = $payload["type"];
+        $ret = call_user_func_array(array("Segment", $type), array($payload));
+        if ($ret) $successful++;
+        $total++;
+        if ($total % 100 === 0) Segment::flush();
+    }
+
+    Segment::flush();
+    unlink($file);
+
+    print("sent $successful from $total requests successfully");
 }
-
-/**
- * File contents.
- */
-
-$contents = file_get_contents($file);
-$lines = explode("\n", $contents);
-
-/**
- * Initialize the client.
- */
-
-Segment::init($args["secret"], array(
-  "debug" => true,
-  "error_handler" => function($code, $msg){
-    print("$code: $msg\n");
-    exit(1);
-  }
-));
-
-/**
- * Payloads
- */
-
-$total = 0;
-$successful = 0;
-foreach ($lines as $line) {
-  if (!trim($line)) continue;
-  $payload = json_decode($line, true);
-  $dt = new DateTime($payload["timestamp"]);
-  $ts = floatval($dt->getTimestamp() . "." . $dt->format("u"));
-  $payload["timestamp"] = $ts;
-  $type = $payload["type"];
-  $ret = call_user_func_array(array("Segment", $type), array($payload));
-  if ($ret) $successful++;
-  $total++;
-  if ($total % 100 === 0) Segment::flush();
-}
-
-Segment::flush();
-unlink($file);
 
 /**
  * Sent
  */
 
-print("sent $successful from $total requests successfully");
 exit(0);
 
 /**
